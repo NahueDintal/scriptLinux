9cd1c6e0ba2a9c6facda6bfe29c6252351a41d34
NaCk3rZ! <nahue.dintal@gmail.com>
2026-03-17 10:18:42 -0300

    cambio minimos en pomodoro

--- pomodoro.sh
+++ pomodoro.sh
@@ -10,11 +10,11 @@ echo -e "Descanso de ${descanso} min"
 
 echo "Inicio"
 sleep ${pomodoro}m
-notify-send "⏰ Temporizador" "¡Pomodoro Terminado!"
+notify-send "⏰ Temporizador" "Pomodoro Terminado!"
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 echo "Fin de pomodoro"
 echo "Decanzo"
 sleep ${descanso}m
-notify-send "⏰ Temporizador" "¡Se acabó el tiempo de descanso!"
+notify-send "⏰ Temporizador" "Termino el tiempo de descanso!"
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 echo "Fin descanso"
